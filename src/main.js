ba4fd76496e2f3fe1cc68ed46bba088393d26a9b
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-19 21:19:10 +0300

    feat: add file validation, and text converter

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
-"use strict";
+'use strict';
 
+const fileInput = document.querySelector('.file-input-js');
+const output = document.querySelector(".output-js");
 
+function validateFile(file) {
+  return file.type === 'text/plain';
+}
+
+async function convertFileToText(file) {
+  let text = null;
+  try {
+    text = await file.text();
+    console.log('File has been converted successfully. ');
+  } catch {
+    console.log('Error, file could not be converted.');
+  }
+
+  return text;
+}
+
+fileInput.addEventListener('change', async () => {
+  let file = null;
+  try {
+    file = await fileInput.files[0];
+    console.log('File is uploaded successfully.');
+  } catch (err) {
+    console.log(err);
+  }
+  if (validateFile(file)) {
+    const text = await convertFileToText(file);
+    output.textContent = text;
+  }
+});
