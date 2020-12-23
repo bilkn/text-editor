b72636054b1ed4904f0957dd5bfcb3f7b3b830d9
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-23 23:00:14 +0300

    feat: add text editor interface

--- src/main.js
+++ src/main.js
@@ -2,14 +2,33 @@
 
 const fileInput = document.querySelector('.file-input-js');
 const output = document.querySelector('.output-js');
+const finderInput = document.querySelector('.finder-input-js');
 const findBtn = document.querySelector('.find-btn-js');
-const finder = document.querySelector('.finder-js');
+const editBtn = document.querySelector('.edit-btn-js');
+const editInput = document.querySelector('.edit-input-js');
 
 findBtn.addEventListener('click', () => {
-  const pattern = finder.value;
+  const pattern = finderInput.value;
   const outputText = output.value;
-  matchWordsWithPattern(textToArray(outputText), pattern);
+  let words = textToArray(outputText);
+  let matches = matchWordsWithPattern(words, pattern);
+  // ! Probably, it's a weak code, it would be improved in the future.
+  editBtn.addEventListener('click', function editBtnCallback() {
+    editBtnHandler(matches, words);
+    editBtn.removeEventListener('click', editBtnCallback);
+  });
 });
+
+function editBtnHandler(matches, words) {
+  const editInputvalue = editInput.value;
+  const outputText = words.join(" ");
+  const regex = new RegExp(editInputvalue, "g");
+  outputText.replace()
+
+  if (!matches) {
+    matches.forEach();
+  }
+}
 
 function validateFile(file) {
   return file.type === 'text/plain';
@@ -48,12 +67,13 @@ function textToArray(text) {
   return text.split(' '); // ! Can be changed.
 }
 
-// Matches the finder pattern with an array of words and returns it.
+// Matches the finderInput value (pattern) with an array of words (from the textarea), and returns a new array of object that includes matched words, and their indexes.
+// ! This function could be removed in the future.
 function matchWordsWithPattern(words, pattern) {
   const regex = new RegExp(pattern, 'g');
-  let matches = words.flat().filter((word) => {
-    console.log(word);
-    return regex.test(word);
-  });
-  console.log(matches);
+  let matches = [];
+  for (let [index, word] of Object.entries(words.flat())) {
+    if (regex.test(word)) matches.push({ word: word, index: index });
+  }
+  return matches;
 }
